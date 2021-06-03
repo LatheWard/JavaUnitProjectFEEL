e1ad9b87b0115db56f207d7c3f465f23133f5d7d
LatheWard <lward21@basecampcodingacademy.org>
2021-06-03 10:39:40 -0500

    Added view playlist feature and overall input vaidation with try and catch (in add feature)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,5 @@
 package com.company;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
     public static Playlist getRandomElement(ArrayList<Playlist> list)
@@ -13,30 +10,36 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        String[] genres = {"Rock", "Alternative", "Jazz", "Country", "Blues", "Pop", "Rap", "Metal"};
+        String[] genres = {"rock", "alternative", "jazz", "country", "blues", "pop", "rap", "metal"};
         String[] emotions ={"happy", "sad", "angry", "anxious"};
         ArrayList<String> genresList = new ArrayList<>(Arrays.asList(genres));
         ArrayList<String> emotionsList = new ArrayList<>(Arrays.asList(emotions));
+        ArrayList<ArrayList> allPlaylists = new ArrayList<>();
         ArrayList<Playlist> happyList = new ArrayList<>();
+        allPlaylists.add(happyList);
         Playlist happySongs = new Playlist("HappySongs", "pop", 221, 734, false);
         happyList.add(happySongs);
 
         ArrayList<Playlist> sadList = new ArrayList<>();
+        allPlaylists.add(sadList);
         Playlist sadSongs = new Playlist("SadSongs", "pop", 207, 720, false);
         sadList.add(sadSongs);
 
         ArrayList<Playlist> angryList = new ArrayList<>();
+        allPlaylists.add(angryList);
+        Playlist REDRUM = new Playlist("REDRUM", "Metal", 99, 109, true);
         Playlist angrySongs = new Playlist("Angry Tunes", "Metal", 36, 139, false);
         angryList.add(angrySongs);
 
         ArrayList<Playlist> anxiousList = new ArrayList<>();
+        allPlaylists.add(anxiousList);
         Playlist anxiousSongs = new Playlist("Anxiety Relief", "Alternative", 336, 252, false);
         anxiousList.add(anxiousSongs);
 
         System.out.println(" - - F E E L - - ");
         String mainChoice = ""; // use for main functioning later
         while(!mainChoice.equalsIgnoreCase("exit")){
-            System.out.println("[help], Enter my [emotions], [add] my Playlists, [exit]");
+            System.out.println("[help], Enter my [emotions], [add] my Playlists, [view] playlists, [exit]");
             mainChoice = userInput.nextLine();
             if(mainChoice.equalsIgnoreCase("help")){
                 System.out.println("ENTER MY EMOTIONS:");
@@ -47,6 +50,10 @@ public class Main {
                 System.out.print(" Along with our own playlists to listen to, you can add in the names and information\n" +
                         " of your playlist as well to work with our algorithm" +
                         "\n- - - -");
+                System.out.println("\nVIEW PLAYLISTS:");
+                System.out.print("You can view all of the playlists we have in production\n" +
+                        " This includes the ones you've entered" +
+                        "\n- - - -");
                 System.out.println("\nEXIT:");
                 System.out.print("Exits the app\n");
             }
@@ -55,6 +62,11 @@ public class Main {
                 System.out.println("Which of these do you feel the strongest right now");
                 System.out.println("Happy, sad, angry, or anxious?");
                 String firstEmotion = userInput.nextLine();
+                while(!emotionsList.contains(firstEmotion)){
+                    System.out.println("Please enter in the closest emotion listed");
+                    System.out.println("Happy, sad, angry, or anxious?");
+                    firstEmotion = userInput.nextLine();
+                }
                 System.out.println("Here's a playlist we think would be great for you: ");
                 if(firstEmotion.equalsIgnoreCase("happy")){
                     System.out.print(getRandomElement(happyList).toString());
@@ -75,7 +87,19 @@ public class Main {
             else if(mainChoice.equalsIgnoreCase("add")){
                 // Playlist making decision
                 System.out.println("How many playlists do you want to enter? Please enter a valid number");
-                int entryNumberOfPlaylists = userInput.nextInt();
+                boolean validAdd = false;
+                int entryNumberOfPlaylists = 0;
+                while(!validAdd){
+                    try{
+                        entryNumberOfPlaylists = userInput.nextInt();
+                        validAdd = true;
+                    }
+                    catch(InputMismatchException e) {
+                        System.out.println("Please enter an integer");
+                        userInput.nextLine();
+                    }
+                }
+
                 userInput.nextLine();
                 String playlistInfo = "";
                 for(int i = entryNumberOfPlaylists; i > 0; i--){
@@ -85,26 +109,41 @@ public class Main {
                     System.out.println("Rock, Alternative, Jazz, Country, Blues, Pop, Rap, Metal \n>");
 
                     String playGenre = userInput.nextLine();
-                    if(!genresList.contains(playGenre)){
+                    while(!genresList.contains(playGenre.toLowerCase(Locale.ROOT))){
                         System.out.println("Please enter one of our genres");
                         System.out.println("Rock, Alternative, Jazz, Country, Blues, Pop, Rap, Metal \n>");
                         playGenre = userInput.nextLine();
                     }
                     System.out.println("How many songs are on the playlist? Please enter a number");
-                    int playNumOfSongs = userInput.nextInt();
+                    boolean validNumOfSongs = false;
+                    int playNumOfSongs = 0;
+                    while(!validNumOfSongs){
+                        try{
+                            playNumOfSongs = userInput.nextInt();
+                            validNumOfSongs = true;
+                        }
+                        catch(InputMismatchException e) {
+                            System.out.println("Please enter an integer");
+                            userInput.nextLine();
+                        }
+                    }
+
                     userInput.nextLine();
                     System.out.println("Is this a favorite? [Y/N] >");
                     boolean playFavoriteUse = false;
-                    String playFavorite = userInput.nextLine();
-                    if(playFavorite.equalsIgnoreCase("y")){
-                        playFavoriteUse = true;
-                    }
-                    else if(playFavorite.equalsIgnoreCase("n")){
-                        continue;
-                    }
-                    else{
-                        System.out.println("Please enter Y or N\n > ");
+                    String playFavorite = "";
+                    while(!playFavorite.equalsIgnoreCase("y") || !playFavorite.equalsIgnoreCase("n")){
                         playFavorite = userInput.nextLine();
+                        if(playFavorite.equalsIgnoreCase("y")){
+                            playFavoriteUse = true;
+                            break;
+                        }
+                        else if(playFavorite.equalsIgnoreCase("n")){
+                            break;
+                        }
+                        else{
+                            System.out.println("Please enter Y or N\n > ");
+                        }
                     }
                     double playLength = Playlist.getLength(playNumOfSongs);
                     Playlist newPlaylist = new Playlist(playName, playGenre, playNumOfSongs, playLength, playFavoriteUse);
@@ -114,9 +153,11 @@ public class Main {
                     }
                     else if(newPlaylist.genre.equalsIgnoreCase("Jazz")){
                         anxiousList.add(newPlaylist);
+                        sadList.add(newPlaylist);
                     }
                     else if(newPlaylist.genre.equalsIgnoreCase("Country")){
                         sadList.add(newPlaylist);
+                        happyList.add(newPlaylist);
                     }
                     else if(newPlaylist.genre.equalsIgnoreCase("Blues")){
                         sadList.add(newPlaylist);
@@ -129,6 +170,14 @@ public class Main {
                         happyList.add(newPlaylist);
                         angryList.add(newPlaylist);
                     }
+                    else if(newPlaylist.genre.equalsIgnoreCase("Alternative")){
+                        anxiousList.add(newPlaylist);
+                        sadList.add(newPlaylist);
+                    }
+                    else if(newPlaylist.genre.equalsIgnoreCase("Metal")){
+                        angryList.add(newPlaylist);
+                        sadList.add(newPlaylist);
+                    }
                     System.out.println("Still want to add more playlists? [Y/N] >");
                     String wantMorePlaylists = userInput.nextLine();
                     if(wantMorePlaylists.equalsIgnoreCase("y")){
@@ -138,6 +187,13 @@ public class Main {
                         break;
                     }
             }
+            }
+            else if(mainChoice.equalsIgnoreCase("view")){
+                for(ArrayList list: allPlaylists){
+                   for(Object playlist : list){
+                       System.out.println(playlist.toString());
+                   }
+                }
             }
         }
     }
